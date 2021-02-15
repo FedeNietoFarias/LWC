15058fe6d4356006c563adb878ba6f2b48aa012e
Fede <federico.nieto@xappia.com>
2021-02-15 10:39:30 -0300

    Pruebas Unitarias

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -3,6 +3,7 @@ import FIRSTNAME_CONTACT from '@salesforce/schema/Contact.FirstName';
 import LASTNAME_CONTACT from '@salesforce/schema/Contact.LastName';
 import EMAIL_CONTACT from '@salesforce/schema/Contact.Email';
 import getContacts from '@salesforce/apex/contactController.getContacts';
+import { reduceErrors } from 'c/ldsUtils';
 const COLUMNS=[
     {label: 'First Name', fieldName: FIRSTNAME_CONTACT.fieldApiName,type:'text'},
     {label:'Last Name',fieldName:LASTNAME_CONTACT.fieldApiName,type:'text'},
@@ -12,4 +13,8 @@ export default class ContactList extends LightningElement {
     columns=COLUMNS;
     @wire(getContacts)
     contacts;
+    get errors() {
+        return (this.accounts.error) ?
+            reduceErrors(this.accounts.error) : [];
+    }
 }
