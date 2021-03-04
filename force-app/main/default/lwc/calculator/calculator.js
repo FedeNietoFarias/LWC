4d6e3ec08734e85542e22c40a8685fa359885a7e
Fede <federico.nieto@xappia.com>
2021-03-03 21:20:13 -0300

    Create a HTML calculator and save on array the numbers

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -1,3 +1,43 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api, track } from 'lwc';
 
-export default class Calculator extends LightningElement {}
+export default class Calculator extends LightningElement {
+    numbers=[];
+    handleClick(event){
+       console.log('ENTROOO');
+       var operator=event.target.label;
+       var number;
+       
+       console.log(operator);
+       if(operator==='/'){
+
+       }
+
+       else if(operator ==='*'){
+
+       }
+
+       else if(operator ==='-'){
+           
+        }
+
+       else if(operator ==='+'){
+           
+        }
+
+       else if(operator ==='+'){
+           
+        }
+
+        else{
+            number = parseInt(operator,10);
+            
+            this.numbers = [...this.numbers, number];
+            
+        }
+        
+        console.log(this.numbers);
+        
+
+    }
+    
+}
